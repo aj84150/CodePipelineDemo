2cfe13d81fa5056869b48dd9b994ac34d5cea7b5
Anoop AR <anooprehman@amway.com>
2019-08-20 12:12:40 +0900

    First commit. Code pipeline demo

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+const app = express()
+
+app.get("/", (req, res) => {
+	res.send('Hello from node server !!!');
+	console.log('Hello from node server !!!');
+});
+
+app.listen(3000, () => {
+	console.log("Server running on port 3000");
+});
